26d0ca451929aee735709f13b61170f207aecdec
Siegfried Bilstein <sbilstein@linkedin.com>
2015-07-17 15:47:52 -0700

    trying to animate cantor

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -8,8 +8,8 @@
         isDrawing = true;
         var squareSize = 5;
         // fill grid of 10px squares with randomly chosen color
-        for(j = 0; j < squareLength; j+=squareSize) {
-          for(i = 0; i < squareLength; i+=squareSize) {
+        for(var j = 0; j < squareLength; j+=squareSize) {
+          for(var i = 0; i < squareLength; i+=squareSize) {
             if(Math.random() < .64) {
               context.fillStyle = '#EE1111';
             } else {
@@ -25,37 +25,62 @@
   }
 
   function cantor(context, squareLength, width, height) {
-    var start = null;
-    function draw(timestamp) {
-      if (!start) start = timestamp;
-      var progress = timestamp - start;
-      // granularity happens in rounds
-      granularity = Math.floor(timestamp / 200) + 1;
-      context.fillStyle = '#222222';
-      var step = width / Math.pow(3, granularity);
-      for(j = 0; j < width; j += step) {
-       context.fillRect(j, 50, 1, 1)
+   var start = null;
+   var toDraw = [];
+
+   function drawSlice(start, end, y, barsize, generation) {
+     var segment_size = Math.max(width * Math.pow(3, -generation), .1);
+     var drawSegment = true;
+     context.clearRect(0,0,width, height);
+     for(var x = start; x < end; x = x + segment_size) {
+       if(drawSegment === true) {
+         context.fillStyle = '#233123';
+         context.fillRect(x, y, segment_size, barsize);
+        }
+        if(drawSegment === true && y < height) {
+          toDraw.push(function() {
+            drawSlice(x, x + segment_size, 0, height, generation + 1);
+          });
+          // this works
+          // drawSlice(x, x + segment_size, y + barsize, barsize, generation + 1);
+        }
+        drawSegment = !drawSegment; 
       }
     }
 
-    return draw;
-  }
-  
-  function shadows(context, squareLength, width, height) {
-    var start = null;
-    var driftRate = 5 / 1000; 
-    function draw(timestamp) {
-      if (!start) start = timestamp;
+    
+
+   function draw(timestamp) { 
+    if (!start) {
+      context.fillStyle = '#DCCEDC';
+      context.fillRect(0, 0, width, height);
+      start = timestamp;
       var progress = timestamp - start;
-      var drift = 3 * Math.sin(progress * ((2 * Math.PI) / 5000));
-      var cosDrift = 3 * Math.cos(progress * ((2 * Math.PI) / 5000));
+        toDraw.push(function(){drawSlice(0, width, 0, height, 0)});
+      } 
+    var slicer = toDraw.pop();
+    slicer();
+   }
 
-      context.clearRect(0, 0, width, height);
 
-      context.fillStyle = '#222222';
-        var squareSize = 10;
-        for(j = 0; j < squareLength ; j+=squareSize) {
-          for(i = 0; i < squareLength * 2; i+= 2 * squareSize) {
+  return draw;
+}
+
+function shadows(context, squareLength, width, height) {
+  var start = null;
+  var driftRate = 5 / 1000; 
+  function draw(timestamp) {
+    if (!start) start = timestamp;
+    var progress = timestamp - start;
+    var drift = 3 * Math.sin(progress * ((2 * Math.PI) / 5000));
+    var cosDrift = 3 * Math.cos(progress * ((2 * Math.PI) / 5000));
+
+    context.clearRect(0, 0, width, height);
+
+    context.fillStyle = '#222222';
+    var squareSize = 10;
+    for(var j = 0; j < squareLength ; j+=squareSize) {
+      for(var i = 0; i < squareLength * 2; i+= 2 * squareSize) {
             // draw black squares at an offset
             var row  = j / squareSize;
             context.fillRect(i - j + drift, j, squareSize, squareSize);
@@ -63,8 +88,8 @@
         }
 
         context.fillStyle = '#292929';
-        for(j = 0; j < squareLength ; j+=squareSize) {
-          for(i = 0; i < squareLength * 2; i+= 2 * squareSize) {
+        for(var j = 0; j < squareLength ; j+=squareSize) {
+          for(var i = 0; i < squareLength * 2; i+= 2 * squareSize) {
             // draw black squares at an offset
             var row  = j / squareSize;
             context.fillRect(i - j, j - drift, squareSize, squareSize);
@@ -72,8 +97,8 @@
         }
 
         context.fillStyle = '#2f2f2f';
-        for(j = 0; j < squareLength ; j+=squareSize) {
-          for(i = 0; i < squareLength * 2; i+= 2 * squareSize) {
+        for(var j = 0; j < squareLength ; j+=squareSize) {
+          for(var i = 0; i < squareLength * 2; i+= 2 * squareSize) {
             // draw black squares at an offset
             var row  = j / squareSize;
             context.fillRect(i - j - cosDrift, j - drift, squareSize, squareSize);
@@ -81,8 +106,8 @@
         }
 
         context.fillStyle = '#f3f3f3';
-        for(j = 0; j < squareLength ; j+=squareSize) {
-          for(i = 0; i < squareLength * 2; i+= 2 * squareSize) {
+        for(var j = 0; j < squareLength ; j+=squareSize) {
+          for(var i = 0; i < squareLength * 2; i+= 2 * squareSize) {
             // draw black squares at an offset
             var row  = j / squareSize;
             context.fillRect(i - j - cosDrift, j + cosDrift, squareSize, squareSize);
@@ -91,32 +116,32 @@
       }
 
       return draw;
-  }
- 
-  function stopDrawing() {
-    isDrawing = false;
-  }
+    }
 
-  function drawGrid(animationFunc) {
-    var canvas = $('#rothko')[0];
-    var context = canvas.getContext('2d');
+    function stopDrawing() {
+      isDrawing = false;
+    }
 
-    var parent = $('#rothko').parent();
-    var squareWidth = parent.width();
-    
+    function drawGrid(animationFunc) {
+      var canvas = $('#rothko')[0];
+      var context = canvas.getContext('2d');
 
-    canvas.width =  squareWidth;
-    canvas.height = squareWidth;  
+      var parent = $('#rothko').parent();
+      var squareWidth = parent.width();
 
-    var squareLength = canvas.width;
 
-    return animationFunc(context, squareLength, canvas.width, canvas.height);
-  }
+      canvas.width =  squareWidth;
+      canvas.height = squareWidth;  
 
-  function animationLoop(timestamp) {
-    functionToDraw(timestamp);
-    window.requestAnimationFrame(animationLoop);
-  };
+      var squareLength = canvas.width;
+
+      return animationFunc(context, squareLength, canvas.width, canvas.height);
+    }
+
+    function animationLoop(timestamp) {
+      functionToDraw(timestamp);
+      window.requestAnimationFrame(animationLoop);
+    };
 
   // Global state variables
   var functionToDraw;
@@ -128,18 +153,28 @@
       functionToBind = morellet;
       functionToDraw = drawGrid(morellet);
       $("#description")
-        .html("The picture above is inspired by a piece of art I saw in the MoMA. View the original <em>Random Distribution of 40,000 Squares Using the Odd and Even Numbers of a Telephone Directory</em> by Francois Morellet over at the <a href=\"https://www.moma.org/m/arts/105479?locale=en\">MoMA Website.</a>");
+      .html("The picture above is inspired by a piece of art I saw in the MoMA. View the original <em>Random Distribution of 40,000 Squares Using the Odd and Even Numbers of a Telephone Directory</em> by Francois Morellet over at the <a href=\"https://www.moma.org/m/arts/105479?locale=en\">MoMA Website.</a>");
+      $("#canvas-title").text("Morellet");
       e.preventDefault();
     });
 
     $("#floater-link").click(function(e) {
       functionToBind = shadows;
       functionToDraw = drawGrid(shadows);
-      $("#description").html("");
+      $("#description").html("Sway.");
+      $("#canvas-title").text("Floater");
       e.preventDefault();
     });
 
-    functionToBind = shadows;
+    $("#cantor-link").click(function(e) {
+      functionToBind = cantor;
+      functionToDraw = drawGrid(cantor);
+      $("#description").html("");
+      $("#canvas-title").text("Cantor");
+      e.preventDefault();
+    });
+
+    functionToBind = cantor;
     functionToDraw = drawGrid(functionToBind);
 
     animationLoop(0);
